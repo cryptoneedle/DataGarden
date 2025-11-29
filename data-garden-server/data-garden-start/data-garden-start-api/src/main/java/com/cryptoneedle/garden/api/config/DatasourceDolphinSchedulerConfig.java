72305d9a51d1fff0f7303d4c8a8246a32859ab7e
CryptoNeedle <cryptoneedle@gmail.com>
2025-11-29 10:17:47 +0800

    fix 移除无用的JPA事务管理工厂

--- data-garden-server/data-garden-start/data-garden-start-api/src/main/java/com/cryptoneedle/garden/api/config/DatasourceDolphinSchedulerConfig.java
+++ data-garden-server/data-garden-start/data-garden-start-api/src/main/java/com/cryptoneedle/garden/api/config/DatasourceDolphinSchedulerConfig.java
@@ -4,18 +4,11 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * <p>description: 配置 DolphinScheduler 数据源 </p>
@@ -24,7 +17,6 @@ import java.util.Map;
  * @date 2025-11-21
  */
 @Configuration
-@EnableTransactionManagement
 @ConditionalOnBooleanProperty(name = "dolphin-scheduler.enabled")
 public class DatasourceDolphinSchedulerConfig {
 
@@ -32,31 +24,6 @@ public class DatasourceDolphinSchedulerConfig {
     @ConfigurationProperties(prefix = "spring.datasource.dolphin-scheduler")
     public DataSource dolphinSchedulerDataSource() {
         return new DruidDataSource();
-    }
-
-    @Bean(name = "dolphinSchedulerEntityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean dolphinSchedulerEntityManagerFactory(
-            EntityManagerFactoryBuilder builder,
-            @Qualifier("dolphinSchedulerDataSource") DataSource dolphinSchedulerDataSource) {
-
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", "none");
-        properties.put("hibernate.show_sql", false);
-
-        return builder
-                .dataSource(dolphinSchedulerDataSource)
-                .packages(
-                        "com.cryptoneedle.garden.infrastructure.entity"
-                )
-                .persistenceUnit("dolphinScheduler")
-                .properties(properties)
-                .build();
-    }
-
-    @Bean(name = "dolphinSchedulerTransactionManager")
-    public PlatformTransactionManager dolphinSchedulerTransactionManager(
-            @Qualifier("dolphinSchedulerEntityManagerFactory") LocalContainerEntityManagerFactoryBean dolphinSchedulerEntityManagerFactory) {
-        return new JpaTransactionManager(dolphinSchedulerEntityManagerFactory.getObject());
     }
 
     @Bean(name = "dolphinSchedulerJdbcTemplate")
--- data-garden-server/data-garden-start/data-garden-start-api/src/main/java/com/cryptoneedle/garden/api/config/DatasourceDorisConfig.java
+++ data-garden-server/data-garden-start/data-garden-start-api/src/main/java/com/cryptoneedle/garden/api/config/DatasourceDorisConfig.java
@@ -4,18 +4,12 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * <p>description: 配置 Doris 数据源 </p>
@@ -32,31 +26,6 @@ public class DatasourceDorisConfig {
     @ConfigurationProperties(prefix = "spring.datasource.doris")
     public DataSource dorisDataSource() {
         return new DruidDataSource();
-    }
-
-    @Bean(name = "dorisEntityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean dorisEntityManagerFactory(
-            EntityManagerFactoryBuilder builder,
-            @Qualifier("dorisDataSource") DataSource dorisDataSource) {
-
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", "none");
-        properties.put("hibernate.show_sql", false);
-
-        return builder
-                .dataSource(dorisDataSource)
-                .packages(
-                        "com.cryptoneedle.garden.infrastructure.entity"
-                )
-                .persistenceUnit("doris")
-                .properties(properties)
-                .build();
-    }
-
-    @Bean(name = "dorisTransactionManager")
-    public PlatformTransactionManager dorisTransactionManager(
-            @Qualifier("dorisEntityManagerFactory") LocalContainerEntityManagerFactoryBean dorisEntityManagerFactory) {
-        return new JpaTransactionManager(dorisEntityManagerFactory.getObject());
     }
 
     @Bean(name = "dorisJdbcTemplate")
